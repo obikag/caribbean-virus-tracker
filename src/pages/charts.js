9ad19fb79bfd641feda381104f8c323e0b855cbe
Obika Gellineau <obikagellineau@gmail.com>
2020-04-24 23:59:28 -0400

    Minor Chart Update V2

--- src/pages/charts.js
+++ src/pages/charts.js
@@ -18,23 +18,23 @@ const ChartsPage = ({data}) => {
         chart: {type: "line"},
         title: {text: 'COVID-19 Cases by Caribbean Country'},
         xAxis: {categories: xDateAxis()},
-        series: [{name: "Antigua and Barbuda", data: yCountryCasesAxis(1)},
-                 {name: "Bahamas", data: yCountryCasesAxis(2)},
-                 {name: "Barbados", data: yCountryCasesAxis(3)},
-                 {name: "Belize", data: yCountryCasesAxis(4)},
-                 {name: "Cuba", data: yCountryCasesAxis(5)},
-                 {name: "Dominica", data: yCountryCasesAxis(6)},
-                 {name: "Dominican Republic", data: yCountryCasesAxis(7)},
-                 {name: "Grenada", data: yCountryCasesAxis(8)},
-                 {name: "Guyana", data: yCountryCasesAxis(9)},
-                 {name: "Haiti", data: yCountryCasesAxis(10)},
-                 {name: "Jamaica", data: yCountryCasesAxis(11)},
-                 {name: "Martinique", data: yCountryCasesAxis(12)},
-                 {name: "Saint Kitts and Nevis", data: yCountryCasesAxis(13)},
-                 {name: "Saint Lucia", data: yCountryCasesAxis(14)},
-                 {name: "Saint Vincent and Grenadines", data: yCountryCasesAxis(15)},
-                 {name: "Suriname", data: yCountryCasesAxis(16)},
-                 {name: "Trinidad and Tobago", data: yCountryCasesAxis(17)},
+        series: [{name: "Antigua and Barbuda", data: yCountryCasesAxis("Antigua and Barbuda")},
+                 {name: "Bahamas", data: yCountryCasesAxis("Bahamas")},
+                 {name: "Barbados", data: yCountryCasesAxis("Barbados")},
+                 {name: "Belize", data: yCountryCasesAxis("Belize")},
+                 {name: "Cuba", data: yCountryCasesAxis("Cuba")},
+                 {name: "Dominica", data: yCountryCasesAxis("Dominica")},
+                 {name: "Dominican Republic", data: yCountryCasesAxis("Dominican Republic")},
+                 {name: "Grenada", data: yCountryCasesAxis("Grenada")},
+                 {name: "Guyana", data: yCountryCasesAxis("Guyana")},
+                 {name: "Haiti", data: yCountryCasesAxis("Haiti")},
+                 {name: "Jamaica", data: yCountryCasesAxis("Jamaica")},
+                 {name: "Martinique", data: yCountryCasesAxis("Martinique")},
+                 {name: "Saint Kitts and Nevis", data: yCountryCasesAxis("Saint Kitts and Nevis")},
+                 {name: "Saint Lucia", data: yCountryCasesAxis("Saint Lucia")},
+                 {name: "Saint Vincent and Grenadines", data: yCountryCasesAxis("Saint Vincent and Grenadines")},
+                 {name: "Suriname", data: yCountryCasesAxis("Suriname")},
+                 {name: "Trinidad and Tobago", data: yCountryCasesAxis("Trinidad and Tobago")},
                 ]
     }
     
@@ -42,23 +42,23 @@ const ChartsPage = ({data}) => {
         chart: {type: "line"},
         title: {text: 'COVID-19 Deaths by Caribbean Country'},
         xAxis: {categories: xDateAxis()},
-        series: [{name: "Antigua and Barbuda", data: yCountryDeathsAxis(1)},
-                 {name: "Bahamas", data: yCountryDeathsAxis(2)},
-                 {name: "Barbados", data: yCountryDeathsAxis(3)},
-                 {name: "Belize", data: yCountryDeathsAxis(4)},
-                 {name: "Cuba", data: yCountryDeathsAxis(5)},
-                 {name: "Dominica", data: yCountryDeathsAxis(6)},
-                 {name: "Dominican Republic", data: yCountryDeathsAxis(7)},
-                 {name: "Grenada", data: yCountryDeathsAxis(8)},
-                 {name: "Guyana", data: yCountryDeathsAxis(9)},
-                 {name: "Haiti", data: yCountryDeathsAxis(10)},
-                 {name: "Jamaica", data: yCountryDeathsAxis(11)},
-                 {name: "Martinique", data: yCountryDeathsAxis(12)},
-                 {name: "Saint Kitts and Nevis", data: yCountryDeathsAxis(13)},
-                 {name: "Saint Lucia", data: yCountryDeathsAxis(14)},
-                 {name: "Saint Vincent and Grenadines", data: yCountryDeathsAxis(15)},
-                 {name: "Suriname", data: yCountryDeathsAxis(16)},
-                 {name: "Trinidad and Tobago", data: yCountryDeathsAxis(17)},
+        series: [{name: "Antigua and Barbuda", data: yCountryDeathsAxis("Antigua and Barbuda")},
+                 {name: "Bahamas", data: yCountryDeathsAxis("Bahamas")},
+                 {name: "Barbados", data: yCountryDeathsAxis("Barbados")},
+                 {name: "Belize", data: yCountryDeathsAxis("Belize")},
+                 {name: "Cuba", data: yCountryDeathsAxis("Cuba")},
+                 {name: "Dominica", data: yCountryDeathsAxis("Dominica")},
+                 {name: "Dominican Republic", data: yCountryDeathsAxis("Dominican Republic")},
+                 {name: "Grenada", data: yCountryDeathsAxis("Grenada")},
+                 {name: "Guyana", data: yCountryDeathsAxis("Guyana")},
+                 {name: "Haiti", data: yCountryDeathsAxis("Haiti")},
+                 {name: "Jamaica", data: yCountryDeathsAxis("Jamaica")},
+                 {name: "Martinique", data: yCountryDeathsAxis("Martinique")},
+                 {name: "Saint Kitts and Nevis", data: yCountryDeathsAxis("Saint Kitts and Nevis")},
+                 {name: "Saint Lucia", data: yCountryDeathsAxis("Saint Lucia")},
+                 {name: "Saint Vincent and Grenadines", data: yCountryDeathsAxis("Saint Vincent and Grenadines")},
+                 {name: "Suriname", data: yCountryDeathsAxis("Suriname")},
+                 {name: "Trinidad and Tobago", data: yCountryDeathsAxis("Trinidad and Tobago")},
                 ]
     }
     
@@ -70,86 +70,21 @@ const ChartsPage = ({data}) => {
         return xValues;
     }
 
-    function yCountryCasesAxis(num){
+    function yCountryCasesAxis(key){
+        key = key.replace(/\s/g, '_');
         let yValues = [];
         data.country_cases.nodes.forEach(node => {
-            if(num === 1){
-                yValues.push(parseInt(node.Antigua_and_Barbuda));
-            }else if (num === 2){
-                yValues.push(parseInt(node.Bahamas));
-            }else if (num === 3){
-                yValues.push(parseInt(node.Barbados));
-            }else if (num === 4){
-                yValues.push(parseInt(node.Belize));
-            }else if (num === 5){
-                yValues.push(parseInt(node.Cuba));
-            }else if (num === 6){
-                yValues.push(parseInt(node.Dominica));
-            }else if (num === 7){
-                yValues.push(parseInt(node.Dominican_Republic));
-            }else if (num === 8){
-                yValues.push(parseInt(node.Grenada));
-            }else if (num === 9){
-                yValues.push(parseInt(node.Guyana));
-            }else if (num === 10){
-                yValues.push(parseInt(node.Haiti));
-            }else if (num === 11){
-                yValues.push(parseInt(node.Jamaica));
-            }else if (num === 12){
-                yValues.push(parseInt(node.Martinique));
-            }else if (num === 13){
-                yValues.push(parseInt(node.Saint_Kitts_and_Nevis));
-            }else if (num === 14){
-                yValues.push(parseInt(node.Saint_Lucia));
-            }else if (num === 15){
-                yValues.push(parseInt(node.Saint_Vincent_and_the_Grenadines));
-            }else if (num === 16){
-                yValues.push(parseInt(node.Suriname));
-            }else if (num === 17){
-                yValues.push(parseInt(node.Trinidad_and_Tobago));
-            }
+            yValues.push(parseInt(node[key]));
         });
         return yValues;
     }
 
-    function yCountryDeathsAxis(num){
+    
+    function yCountryDeathsAxis(key){
+        key = key.replace(/\s/g, '_');
         let yValues = [];
         data.country_deaths.nodes.forEach(node => {
-            if(num === 1){
-                yValues.push(parseInt(node.Antigua_and_Barbuda));
-            }else if (num === 2){
-                yValues.push(parseInt(node.Bahamas));
-            }else if (num === 3){
-                yValues.push(parseInt(node.Barbados));
-            }else if (num === 4){
-                yValues.push(parseInt(node.Belize));
-            }else if (num === 5){
-                yValues.push(parseInt(node.Cuba));
-            }else if (num === 6){
-                yValues.push(parseInt(node.Dominica));
-            }else if (num === 7){
-                yValues.push(parseInt(node.Dominican_Republic));
-            }else if (num === 8){
-                yValues.push(parseInt(node.Grenada));
-            }else if (num === 9){
-                yValues.push(parseInt(node.Guyana));
-            }else if (num === 10){
-                yValues.push(parseInt(node.Haiti));
-            }else if (num === 11){
-                yValues.push(parseInt(node.Jamaica));
-            }else if (num === 12){
-                yValues.push(parseInt(node.Martinique));
-            }else if (num === 13){
-                yValues.push(parseInt(node.Saint_Kitts_and_Nevis));
-            }else if (num === 14){
-                yValues.push(parseInt(node.Saint_Lucia));
-            }else if (num === 15){
-                yValues.push(parseInt(node.Saint_Vincent_and_the_Grenadines));
-            }else if (num === 16){
-                yValues.push(parseInt(node.Suriname));
-            }else if (num === 17){
-                yValues.push(parseInt(node.Trinidad_and_Tobago));
-            }
+            yValues.push(parseInt(node[key]));
         });
         return yValues;
     }
